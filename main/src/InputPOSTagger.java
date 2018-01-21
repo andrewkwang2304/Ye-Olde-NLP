fc124f925b119cba13112185bf43b3568851feb1
Andrew Kwang <akwang@ucsc.edu>
2018-01-20 16:38:00 -0800

    Bring us the girl, wipe away the debt, Booker De Witt.

--- main/src/InputPOSTagger.java
+++ main/src/InputPOSTagger.java
@@ -1,18 +1,41 @@
 // This program will parse the user input and tag the proper POS.
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.FileInputStream;
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class InputPOSTagger {
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-        try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
-            POSModel model = new POSModel(modelIn);
-        }
+    public static void main(String[] args) throws IOException {
+        POSModel model = new POSModelLoader().load(new File("en-pos-maxent.bin"));
+        PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
         POSTaggerME tagger = new POSTaggerME(model);
+
+        // We'll need to take input from the cmd at some point, but this will do for now.
+        String input = "Can anyone help me dig through OpenNLP's horrible documentation?";
+        ObjectStream<String> lineStream =
+                new PlainTextByLineStream(new StringReader(input));
+
+        perfMon.start();
+        String line;
+        while ((line = lineStream.read()) != null) {
+
+            String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
+            String[] tags = tagger.tag(whitespaceTokenizerLine);
+
+            POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
+            System.out.println(sample.toString());
+
+            perfMon.incrementCounter();
+        }
+        perfMon.stopAndPrintFinalResult();
     }
 }
-
