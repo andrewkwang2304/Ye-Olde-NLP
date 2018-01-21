e39454ff215c791d166f755f521649825ede6239
Andrew Kwang <akwang@ucsc.edu>
2018-01-20 16:24:40 -0800

    We don't need the src files

--- main/src/InputPOSTagger.java
+++ main/src/InputPOSTagger.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
 import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 
 public class InputPOSTagger {
     public static void main(String[] args) throws FileNotFoundException, IOException{
         try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
             POSModel model = new POSModel(modelIn);
         }
+        POSTaggerME tagger = new POSTaggerME(model);
     }
 }
 
